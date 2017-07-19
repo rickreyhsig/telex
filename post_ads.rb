720273411e41c7ca34a7af45a6abdcbd93fdcb36
Ricardo Kreyhsig <rkreyhsig@gditweb.com>
2017-07-18 21:46:15 -0400

    Watir script updates

--- post_ads.rb
+++ post_ads.rb
@@ -10,22 +10,30 @@ browser.goto 'http://brasiltelexbit.com/publicar.php'
 browser.text_field(name: 'usernames[]').set user
 browser.input(:type =>'submit').click
 anuncio_text = browser.link(:id => 'ad'+user+'0').text
-p anuncio_text.inspect
 
 browser.goto 'https://office.telexbit.com/'
 browser.text_field(name: 'username').set cnf['user']
 browser.text_field(name: 'password').set cnf['password']
-sleep 30
+sleep 13 # Give use time to do captcha
+browser.button(type: 'submit').click
 
-#sleep 20000
-# Give use time to do captcha
-browser.link(:text =>'3 - Valide seus anúncios').when_present.click
+#browser.link(:text =>'3 - Valide seus anúncios').click
+browser.goto 'https://office.telexbit.com/Ads/AdsSelectIII'
+sleep 15
 
-browser.text_field(id: 'site_ads').set anuncio_text
-browser.text_field(id: 'link_ads').set anuncio_text
+[0,1,2,3,4].each do |num|
+	browser.select_list(:name, "n_adcentral").select_value("0")
+	browser.text_field(id: 'site_ads').set anuncio_text
+	browser.text_field(id: 'link_ads').set anuncio_text+num.to_s
+	browser.button(type: 'submit').click
+	sleep 3
+	p "#{num} --- \xE2\x9C\x94"
+	browser.goto 'https://office.telexbit.com/Ads/AdsSelectIII'
+end
 
+browser.goto 'https://office.telexbit.com/Ads/MyAdcentral'
 
-sleep 20
+sleep 10
 
-#puts browser.title
-#browser.close
+puts browser.title
+browser.close
