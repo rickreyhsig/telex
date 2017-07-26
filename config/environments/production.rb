d0b06fad78edb30498cfe1e1ed7295e4203ca077
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2017-07-26 00:00:19 -0400

    Trying chrome binary config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Telex::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  chrome_bin = '/app/.apt/usr/bin/google-chrome'
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.setBinary("/app/.apt/usr/bin/google-chrome");
-  driver = Selenium::WebDriver.for :chrome, options: options
+  #chrome_bin = '/app/.apt/usr/bin/google-chrome'
+  #options = Selenium::WebDriver::Chrome::Options.new
+  #options.setBinary("/app/.apt/usr/bin/google-chrome");
+  #driver = Selenium::WebDriver.for :chrome, options: options
 end
